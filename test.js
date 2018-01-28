0851561acca109e43574b813b59116cbc640d95c
Jake See <jakesee@gmail.com>
2018-01-28 22:01:06 +0800

    added test

---
+++ test.js
@@ -0,0 +1,21 @@
+'use strict';
+
+var _ = require('lodash');
+var columnify = require('columnify');
+var wait = require('wait-for-stuff');
+var tick = require('animation-loops');
+
+var binance = require('binance');
+const rest = new binance.BinanceRest({
+	key: process.env.key,
+	secret: process.env.secret,
+	timeout: 15000,
+	recvWindow: 10000,
+	disableBeautification: false,
+});
+
+const binanceWS = new binance.BinanceWS();
+
+binanceWS.onKline('VENBTC', '1m', (data) => {
+	console.log(data);
+});
