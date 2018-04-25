d1b5cfeb3a25e47a7e768f007b969d6c4085565e
Jake See <jakesee@gmail.com>
2018-04-25 16:48:03 +0800

    added socket.io

--- server.js
+++ server.js
@@ -1,14 +1,26 @@
 const express = require('express');
-const app = express();
-const path = require('path');
+const server = express();
+const path = require('path');\
+const SocketIO = require('socket.io');
 
-app.use(express.static(__dirname + '/dist'));
+server.use(express.static(__dirname + '/dist'));
 
 var port = process.env.PORT || 4434;
-app.listen(port);
+var index = __dirname + '/dist/index.html';
 
-app.get('/*', function(request, response) {
-    response.sendFile(path.join(__dirname + '/dist/index.html'));
+server.listen(port);
+
+server.get('/*', function(request, response) {
+    response.sendFile(path.join(index));
 });
 
 console.log('Listening on', port);
+
+const io = SocketIO(server);
+
+io.on('connection', (socket) => {
+    console.log('client connected');
+    socket.on('disconnect', () => console.log('client disconnected'));
+});
+
+setInterval(() => io.emit('time', new Date().toTimeString()), 1000);
